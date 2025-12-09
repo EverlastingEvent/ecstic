9db1bd35013695eaf8cf348b62fea65fe2af93fd
EverlastingEvent <everlastingevent@mail.ru>
2025-12-10 01:09:22 +0700

    добавлен тест на неравество 2 сущностей

--- tests/basic_test.rs
+++ tests/basic_test.rs
@@ -1,8 +1,18 @@
-use my_ecs::{World, Entity};
+use my_ecs::{Entity, World};
 
 #[test]
 fn test_spawn_despawn() {
     let mut world = World::new();
     let entity = world.spawn();
     assert!(world.despawn(entity));
+}
+
+#[test]
+fn test_multy_spawn() {
+    let mut world = World::new();
+    let entity1 = world.spawn();
+    let entity2 = world.spawn();
+    assert_ne!(entity1, entity2);
+    assert!(world.despawn(entity1));
+    assert!(world.despawn(entity2));
 }
